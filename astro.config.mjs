d29128214b4ed4c0b1c27b8baeb75be4c80aa1f5
AlbeTorres <albertocorreoficial@gmail.com>
2025-04-13 10:00:07 -0400

    chore: delete deprecated astro image dependencie that prevent the project to run

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import NetlifyCMS from "astro-netlify-cms";
-import dcapConfig from "./decap.config.mjs";
-import astropodConfig from "./.astropod/astropod.config.json";
 import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
+import astropodConfig from "./.astropod/astropod.config.json";
+import dcapConfig from "./decap.config.mjs";
 
 // https://astro.build/config
-import image from "@astrojs/image";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,11 +26,6 @@ export default defineConfig({
     mdx(),
     sitemap(),
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-      cacheDir: "./.cache/image",
-      logLevel: "debug",
-    }),
     NetlifyCMS({
       config: dcapConfig(),
     }),
